2fe44ac73a79a3166b3c9c14b5baf9fcc528a169
guangzhixie <guangzhi.xie@berkeley.edu>
2018-09-18 15:23:54 +0800

    fix issue

--- saber/funcs/impl/bm/device/bmkernel_base.c
+++ saber/funcs/impl/bm/device/bmkernel_base.c
@@ -13,13 +13,15 @@ int bmkernel_func(void *args)
 {
     bmkernel_api_base* param = (bmkernel_api_base *)args;
     switch (param->op) {
-        case ACTIVATION:
+        case ACTIVATION: {
             // bm_activation_fwd(param)
             break;
-        case CONV:
+        }
+        case CONV: {
             bm_api_conv_forward* api = (bm_api_conv_forward *)param->opParam;
             bm_conv_fwd(*api);
             break;
+        }
         default:
             printf("op %d is not supported by BM yet.\n", param->op);
     }
