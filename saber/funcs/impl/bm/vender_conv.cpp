e3edbc00a4cfad5993c85227f4ef633a2ed935f7
guangzhixie <guangzhi.xie@berkeley.edu>
2018-09-21 16:39:18 +0800

    test

--- saber/funcs/impl/bm/vender_conv.cpp
+++ saber/funcs/impl/bm/vender_conv.cpp
@@ -262,7 +262,10 @@ SaberStatus VenderConv2D<BM, AK_FLOAT>::\
       secs_info.hsecs
     };
 
-    bm_status_t bm_stat = bmkernel_launch(_handle, "/usr/local/include/bm/bmkernel_bin.bin");
+    bm_handle_t handle = NULL;
+    bmkernel_init(&handle);
+
+    bm_status_t bm_stat = bmkernel_launch(handle, "/usr/local/include/bm/bmkernel_bin.bin");
     CHECK_EQ(BM_SUCCESS, bm_stat) << "bmkernel_launch failed.";
 
     LOG(INFO)<<"BM Conv starts...";
@@ -270,9 +273,11 @@ SaberStatus VenderConv2D<BM, AK_FLOAT>::\
     /* Send arguments. */
     enum BmOpType op = CONV;
     bmkernel_api_base api = { op, reinterpret_cast<void *>(&bm_conv_param) };
-    BM_CHECK(bmkernel_send_args(_handle, reinterpret_cast<void *>(&api), sizeof(api)));
+    BM_CHECK(bmkernel_send_args(handle, reinterpret_cast<void *>(&api), sizeof(api)));
 
     LOG(INFO)<<"BM Conv ends...";
+
+    bmkernel_deinit(handle);
 
     return SaberSuccess;
 }
